0f377304a4d414a21056116b003c2f96d6430c53
Varun Patil <radialapps@gmail.com>
2019-02-04 16:23:37 +0530

    Migrate to AndroidX (fix #231)

--- app/src/main/java/app/insti/adapter/UpVotesAdapter.java
+++ app/src/main/java/app/insti/adapter/UpVotesAdapter.java
@@ -1,10 +1,10 @@
 package app.insti.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
